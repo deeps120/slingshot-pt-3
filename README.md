ddb17c8128a8cd7b88194a592d2f590effe8fac0
deeps120 <62687789+deeps120@users.noreply.github.com>
2020-05-17 12:28:08 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# slingshot-pt-3
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ ball.js
@@ -0,0 +1,23 @@
+class Ball{
+    constructor(x,y,width,height){
+        var options={
+            restitution:2.0,
+            density:0.3,
+            friction:0.3
+        }
+        this.body=Bodies.circle(x,y,width,height);
+        this.width=width;
+        this.height=height;
+        World.add(world,this.body);
+        
+    }
+    display(){
+        var pos= this.body.position;
+        fill("aqua");
+        ellipse(pos.x,pos.y,this.body.width,this.body.height);
+      //  this.body.position.x = mouseX;
+        //this.body.position.y = mouseY;
+        
+    }
+    
+}
